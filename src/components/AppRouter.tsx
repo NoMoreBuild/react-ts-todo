0d5b7924368ed12c7af8726d88943eec44491dc1
NoMoreBuild <rainha1005@gmail.com>
2020-12-06 17:32:01 +0900

    feature: 라우터 설정, 폴더구조 정리

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home'
+
+const AppRouter: React.FC = () => {
+  return(
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+      </Switch>
+    </HashRouter>
+  )
+}
+
+export default AppRouter
