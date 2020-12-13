b8ceee810d0ba3db93855af47a29c8d71d6d0cfb
NoMoreBuild <rainha1005@gmail.com>
2020-12-13 15:25:05 +0900

    feature: 삭제 기능 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 
-let toDoArray: any[] = []
-
 const Home: React.FC = () => {
     const [toDoInput, setToDoInput] = useState<string>("") 
-    let [isLoaded, setIsLoaded] = useState<boolean>(false)
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
+    const [toDoArray, setToDoArray] = useState<any>([])
 
     const loadToDo = () => {
         const getToDo = localStorage.getItem('toDoList');
         if (getToDo !== null){
             const parsedToDo = JSON.parse(getToDo);
-            toDoArray = parsedToDo;
+            setToDoArray(parsedToDo);
             setIsLoaded(true)
         }
     }
@@ -37,6 +36,13 @@ const Home: React.FC = () => {
             setToDoInput(value)
         }
     }
+
+    const onDelete = (event: any) => {
+        const { value } = event.target;
+        toDoArray.splice(value, 1);
+        localStorage.setItem('toDoList', JSON.stringify(toDoArray));
+        loadToDo()
+    }
     
     return(
         <>
@@ -44,11 +50,11 @@ const Home: React.FC = () => {
                 <input type="text" maxLength={120} onChange={onChange} name="toDoInput" value={toDoInput}></input>
                 <input type="submit"></input>
             </form>
-            {isLoaded ? toDoArray.map((todo:any) => {
+            {isLoaded ? toDoArray.map((todo:any, idx:any ) => {
                 return (
                     <div key={todo.id}>
                         {todo.value}
-                        <button>삭제</button>
+                        <button value={idx} onClick={onDelete}>삭제</button>
                     </div>
                 )
             }): <div>isLoading...</div>}
